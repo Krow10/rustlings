a5fe7140d7694ce61284a1f919057d428001ec5e
Krow10 <23462475+Krow10@users.noreply.github.com>
2023-10-17 08:31:08 -0400

    [03] If - Solutions
    1. Write `a > b` condition using ternary expression
    2. Use `if / else if / else` construct
    3. Fix type issues for the `identifier`
       Also added cleaner solutions

--- exercises/03_if/if3.rs
+++ exercises/03_if/if3.rs
@@ -2,17 +2,15 @@
 //
 // Execute `rustlings hint if3` or use the `hint` watch subcommand for a hint.
 
-// I AM NOT DONE
-
 pub fn animal_habitat(animal: &str) -> &'static str {
     let identifier = if animal == "crab" {
         1
     } else if animal == "gopher" {
-        2.0
+        2 // Fix type to use integer
     } else if animal == "snake" {
         3
     } else {
-        "Unknown"
+        -1 // Return value that is not matched in `habitat` test cases below
     };
 
     // DO NOT CHANGE THIS STATEMENT BELOW
@@ -27,6 +25,37 @@ pub fn animal_habitat(animal: &str) -> &'static str {
     };
 
     habitat
+
+    /*
+    Horrible code BTW. Here's a version that uses `match`, keeping the identifier use:
+    
+    let identifier = match animal {
+        "crab" => 1,
+        "gopher" => 2,
+        "snake" => 3,
+        _ => -1,
+    };
+
+    let habitat = match identifier {
+        1 => "Beach",
+        2 => "Burrow",
+        3 => "Desert",
+        _ => "Unknown",
+    };
+
+    habitat
+    */
+
+    /*
+    Or using only one `match`:
+    
+    match animal {
+        "crab" => "Beach",
+        "gopher" => "Burrow",
+        "snake" => "Desert",
+        _ => "Unknown",
+    }
+    */
 }
 
 // No test changes needed.
